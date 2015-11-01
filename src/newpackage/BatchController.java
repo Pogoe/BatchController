703a5978c36ea2da4453fa9f72c0398669ef4b33
jacqueskwizera <mr.jakobo@gmail.com>
2015-11-01 22:19:40 +0100

    Classes

---
+++ src/newpackage/BatchController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import GreenhouseAPI.Greenhouse;
+import GreenhouseAPI.IGreenhouse;
+import PLCCommunication.PLCConnection;
+import PLCCommunication.UDPConnection;
+import java.util.BitSet;
+
+/**
+ *
+ * @author jacqueskwizera
+ */
+public class BatchController implements iBatchController{
+
+    PLCConnection con = new UDPConnection(5000, "192.168.0.10");
+    IGreenhouse api = new Greenhouse(con);
+
+    
+    public double getTemp1(){        
+        return api.ReadTemp1();
+    }
+    
+    public double getTemp2(){
+        return api.ReadTemp2();
+    
+    }
+    
+    public double getMoist(){
+        return api.ReadMoist();
+    }
+    
+    public double getWaterLevel(){
+        return api.ReadWaterLevel();
+    }
+    
+    public BitSet getErros(){
+        BitSet bitSet = api.ReadErrors();
+        //TODO: Konverter bitset til Error besked/klasse.
+        return bitSet;
+    }
+
+    public void setTemp(int kelvin) {
+        api.SetTemperature(kelvin);
+    }
+
+    public void setMoist(int moist) {
+        api.SetMoisture(moist);
+    }
+
+    public void addWater(int sec) {
+        api.AddWater(sec);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/newpackage/iBatchController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author jacqueskwizera
+ */
+public interface iBatchController {
+    public double getTemp1();
+    public double getTemp2();
+    public double getMoist();
+    public double getWaterLevel();
+    public void setTemp(int kelvin);
+    public void setMoist(int moist);
+    public void addWater(int sec);   
+}
